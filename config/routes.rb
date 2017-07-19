e6e196b979212ed6fd354353a21438591bf9d5d0
Wil Donovan <wil.donovan@gmail.com>
2017-07-19 13:06:14 -0600

    route changes

--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,18 @@
 Rails.application.routes.draw do
   root to: 'dashboards#show'
 
-get '/login', to: 'sessions#new'
-get '/logout', to: 'sessions#destroy'
-post '/login', to: 'sessions#create'
+  get '/login', to: 'sessions#new'
+  get '/logout', to: 'sessions#destroy'
+  post '/login', to: 'sessions#create'
 
-get '/signup', to: 'users#new'
-post '/users', to: 'users#create'
+  get '/signup', to: 'users#new'
+  post '/users', to: 'users#create'
 
-resources :companies
-
-resources :residences
-
-resources :transactions
+  namespace :admin do
+    resources :companies
+    resources :residences
+    resources :transactions
+    resources :feeder_cows, except: :show
+    resources :calving_cows, except: :show
+  end
 end
