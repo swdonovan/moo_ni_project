042bcfed9a9d800c0d45f92b4570405e646fe1d0
Ashish Vaid <vaidashi@Ashishs-MacBook-Air.local>
2017-07-22 14:33:54 -0600

    styling requirement fixed

--- db/schema.rb
+++ db/schema.rb
@@ -65,8 +65,8 @@ ActiveRecord::Schema.define(version: 20170721135624) do
     t.integer "max_capacity"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.integer "occupancy", default: 0
     t.string "image_path"
+    t.integer "occupancy", default: 0
   end
 
   create_table "users", force: :cascade do |t|
